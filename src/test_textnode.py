5ab8f5f2e405d33480a12ba1af00e0fc0f2b098c
FG-GIS <flore.giovanni.scripting@outlook.com>
2025-03-15 21:49:46 +0100

    chapter 2: Text Nodes-Unit Test

---
+++ src/test_textnode.py
@@ -0,0 +1,30 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+    
+    def test_eq_more(self):
+        node = TextNode("This is a test node", TextType.CODE,"https://www.boot.dev")
+        node2 = TextNode("This is a test node", TextType.CODE,"https://www.boot.dev")
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node = TextNode("This is a test node", TextType.CODE,"https://www.boot.dev")
+        node2 = TextNode("This is a different test node", TextType.CODE,"https://www.boot.dev")
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_more(self):
+        node3 = TextNode("This is a test node", TextType.CODE,"https://www.boot.dev")
+        node4 = TextNode("This is a test node", TextType.ITALIC,"https://www.boot.dev")
+        self.assertNotEqual(node3, node4)
+
+
+if __name__ == "__main__":
+    unittest.main()
